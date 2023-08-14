803e61dc19c84fcd717569e6561858aeb0d3703a
adjarriawan <muhammad.rizky.adjarriawan@gmail.com>
2023-08-14 23:05:24 +0800

    migration

---
+++ database/migrations/2023_08_14_145012_create_table_product.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('category');
+            $table->decimal('price', 12,2);
+            $table->unsignedBigInteger('supllier_id');
+            $table->timestamps();
+
+            $table->foreign('supllier_id')->references('id')
+                ->on('supllier')->onUpdate('cascade')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
