0d5b900443bd59428661acb09219c76a4f184915
nangapankk <bayhyuga@gmail.com>
2023-08-18 22:58:36 +0800

    fix migration & model

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -11,7 +11,14 @@ class Invoice extends Model
 
     protected $table = 'invoice';
 
+    protected $fillable = [
+        'invoice_number', 'invoice_date', 'invoice_total', 'user_id'
+    ];
+
+    // invoice_detail adalah nama relasi untuk eloquent
     public function invoice_detail(){
+        // invoice_id : ini relasi kolom pada tabel invoice_detail
+        // id : primary key dari tabel invoice
         return $this->hasMany(InvoiceDetail::class, 'invoice_id', 'id');
     }
 }
--- app/Models/InvoiceDetail.php
+++ app/Models/InvoiceDetail.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class InvoiceDetail extends Model
 {
     use HasFactory;
+
+    protected $table = 'invoice_detail';
+    protected $fillable = [
+        'invoice_id', 'product_id', 'quantity', 'discount', 'total'
+    ];
+
+    public function invoice()
+    {
+        return $this->belongsTo(Invoice::class, 'invoice_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
 }
